a9c6352861e70d7ab669113b48b246458ee6034f
Manish Kudtarkar <140542317+ManishKudtarkar@users.noreply.github.com>
2025-06-30 10:36:38 +0530

    30-06-2025

---
+++ prime number.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath> 
+bool isPrime(int num) {
+   
+    if (num <= 1) {
+        return false;
+    }
+    if (num == 2) {
+        return true;
+    }
+    if (num % 2 == 0) {
+        return false;
+    }
+    for (int i = 3; i <= sqrt(num); i += 2) {
+        if (num % i == 0) {
+            return false; 
+        }
+    }
+    return true; 
+}
+
+int main() {
+    int number;
+    std::cout << "Enter a positive integer: ";
+    std::cin >> number;
+
+    if (isPrime(number)) {
+        std::cout << number << " is a prime number." << std::endl;
+    } else {
+        std::cout << number << " is not a prime number." << std::endl;
+    }
+
+    return 0;
+}
